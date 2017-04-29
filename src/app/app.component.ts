fc864c295af02fe72c4de22a3bfca3d9bbd23970
mdk <mdk@lostindetails.com>
2017-04-29 21:27:04 +0200

    added sample usage of HotkeysService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { HotkeysService, Hotkey } from 'angular2-hotkeys';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app works!';
+  hotkeyCtrlLeft: Hotkey | Hotkey[];
+  hotkeyCtrlRight: Hotkey | Hotkey[];
+
+  constructor(private hotkeysService: HotkeysService) {
+    this.hotkeyCtrlLeft = hotkeysService.add(new Hotkey('ctrl+left', this.ctrlLeftPressed));
+    this.hotkeyCtrlRight = hotkeysService.add(new Hotkey('ctrl+right', this.ctrlRightPressed));
+  }
+
+  ctrlLeftPressed = (event: KeyboardEvent, combo: string): boolean => {
+    this.title = 'ctrl+left pressed';
+    return true;
+  }
+
+  ctrlRightPressed = (event: KeyboardEvent, combo: string): boolean => {
+    this.title = 'ctrl+right pressed';
+    return true;
+  }
+
+  ngOnDestroy() {
+    this.hotkeysService.remove(this.hotkeyCtrlLeft);
+    this.hotkeysService.remove(this.hotkeyCtrlRight);
+  }
 }
